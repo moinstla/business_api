f69f02a2e66bbe432fd81b59481ab939ac784f09
Monique St. Laurent <moinstla@gmail.com>
2017-07-21 13:14:04 -0700

    add crud

--- app/controllers/businesses_controller.rb
+++ app/controllers/businesses_controller.rb
@@ -2,11 +2,36 @@ class BusinessesController < ApplicationController
 
   def index
     @businesses = Business.all
-    json_response(@quotes)
+    json_response(@businesses)
   end
+
+  def show
+      @businesse = Business.find(params[:id])
+      json_response(@businesses)
+    end
+
+    def create
+      @businesses = Business.create(businesse_params)
+      json_response(@businesses)
+    end
+
+    def update
+      @businesses = Business.find(params[:id])
+      @businesses.update(businesse_params)
+    end
+
+    def destroy
+      @businesses = Business.find(params[:id])
+      @businesses.destroy
+    end
+
 
   private
   def json_response(object)
     render json: object, status: :ok
+  end
+
+  def business_params
+    params.permit(:name, :business_type, :address)
   end
 end
